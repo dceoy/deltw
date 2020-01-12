1bd05de447354038c3c312035832f7d0e0288910
dceoy <dnarsil+github@gmail.com>
2020-01-13 00:59:18 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,26 +10,24 @@ with open('README.md', 'r') as f:
 setup(
     name='deltw',
     version=__version__,
-    description='Twitter Cleaner to delete archived tweets',
-    packages=find_packages(),
     author='Daichi Narushima',
     author_email='dnarsil+github@gmail.com',
+    description='Twitter Cleaner to delete archived tweets',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/dceoy/deltw',
-    install_requires=[
-        'requests_oauthlib', 'pyyaml'
-    ],
-    entry_points={
-        'console_scripts': ['deltw=deltw.main:main'],
-    },
+    packages=find_packages(),
+    install_requires=['requests_oauthlib', 'pyyaml'],
+    entry_points={'console_scripts': ['deltw=deltw.main:main']},
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS',
         'Programming Language :: Python :: 3',
         'Topic :: Internet'
     ],
-    python_requires='>=3.6',
-    long_description=long_description
+    python_requires='>=3.5'
 )
